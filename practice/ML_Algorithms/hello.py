47f581a72437eda4140cbb5353b869d28ec04379
sheshank <sheshank1436@gmail.com>
2023-02-02 19:34:43 +0530

    added class

--- practice/ML_Algorithms/hello.py
+++ practice/ML_Algorithms/hello.py
@@ -2,4 +2,13 @@ print('hello world')
 print("hello python")
 print('oh my god')
 print('this is the third change in the code ')
-print('making changes to the previous version to add a new tag")
+def add_func():
+    print('function:')
+add_func()
+class name:
+    def __init__(self):
+        self.age=20
+    def dis(self):
+        return self.age
+x=name()
+print(x.dis)
